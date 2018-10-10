daffd5e0c22701edc5b2daf3fa2cd37dc1e01863
SurgeArrester <cameron.h@rgreaves.me.uk>
2018-10-10 12:26:02 +0100

    update gitignore

--- Book/Chapter12/main.py
+++ Book/Chapter12/main.py
@@ -5,16 +5,17 @@ import numpy as np
 os.chdir('/home/cameron/Dropbox/University/PhD/Teaching/COMP219-AI/COMP219/Book/Chapter12/')
 
 def load_mnist(path, kind='train'):
-
     """Load MNIST data from path"""
-    labels_path = os.path.join(path,'%s-label-idx1-ubyte' % kind)
+    labels_path = os.path.join(path,
+                               '%s-labels-idx1-ubyte' % kind)
     images_path = os.path.join(path,
                                '%s-images-idx3-ubyte' % kind)
+    print(os.getcwd())
     print(labels_path)
     with open(labels_path, 'rb') as lbpath:
         magic, n = struct.unpack('>II',  # big endian two unsigned ints
                                  lbpath.read(8))
-        labels = np.fromfile(lbpath, dtype=uint8)
+        labels = np.fromfile(lbpath, dtype='uint8')
 
     with open(images_path, 'rb') as imgpath:
         magic, num, rows, cols = struct.unpack(">IIII",
